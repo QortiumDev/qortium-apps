c58a610e37cc46ce597e7ed01a5186fba69d3641
7r15 <287508559+7r15@users.noreply.github.com>
2026-07-15 18:59:53 -0400

    feat: run favorite notification sync app-wide via Layout

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,28 @@
-import { createHashRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { createHashRouter, RouterProvider, Outlet, useLocation } from 'react-router-dom';
 import { TopBar } from '../components/layout/TopBar';
 import { BrowsePage } from '../pages/BrowsePage';
 import { FavoritesPage } from '../pages/FavoritesPage';
 import { useIframe } from '../hooks/useIframeListener';
+import { useFavoritesNotifications } from '../hooks/useFavoritesNotifications';
+
+const LAST_ROUTE_KEY = 'brs-last-route';
 
 const _startRoute = new URLSearchParams(window.location.search).get('_route');
-if (_startRoute) window.location.hash = _startRoute;
+if (_startRoute) {
+  window.location.hash = _startRoute;
+} else if (!window.location.hash || window.location.hash === '#' || window.location.hash === '#/') {
+  const saved = localStorage.getItem(LAST_ROUTE_KEY);
+  if (saved && saved !== '/') window.location.hash = saved;
+}
 
 function Layout() {
   useIframe();
+  useFavoritesNotifications();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    localStorage.setItem(LAST_ROUTE_KEY, pathname);
+  }, [pathname]);
   return (
     <>
       <TopBar />
